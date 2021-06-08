cb46f9b4c93bf21765d2635ace14b2bcf6e66887
springframeworkHamza <kizotomandera@hotmail.com>
2021-06-08 15:23:59 +0200

    update jpa user

--- ssc-brewery-ua-assn-custom-auth-annotation/src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
+++ ssc-brewery-ua-assn-custom-auth-annotation/src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
@@ -34,23 +34,8 @@ public class JpaUserDetailsService implements UserDetailsService {
 
         log.debug("Getting User info via JPA");
 
-        User user = userRepository.findByUsername(username).orElseThrow(() -> {
+        return userRepository.findByUsername(username).orElseThrow(() -> {
             return new UsernameNotFoundException("User name: " + username + " not found");
         });
-
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-                user.getEnabled(), user.getAccountNonExpired(), user.getCredentialsNonExpired(),
-                user.getAccountNonLocked(), convertToSpringAuthorities(user.getAuthorities()));
-    }
-
-    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
-        if (authorities != null && authorities.size() > 0){
-            return authorities.stream()
-                    .map(Authority::getPermission)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toSet());
-        } else {
-            return new HashSet<>();
-        }
     }
 }
